a869390aa95aef991e6809f74561826b4031f0db
Dima Logvinets <mister.dayvord@gmail.com>
2022-03-23 19:44:22 +0300

    Добавьте файлы проекта.

---
+++ CalculatingsAgain/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CalculatingsAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ft = 14;
+            Console.WriteLine(ft/2+14*(29-13+(14/7)));
+            Console.Read();
+        }
+    }
+}
